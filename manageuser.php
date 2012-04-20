cba8368edf4670008839fded9003cecd9c89b78b
Philipp Kiszka <info@o-dyn.de>
2012-04-20 03:03:48 +0200

    Signed-off-by: Philipp Kiszka <info@o-dyn.de>

--- manageuser.php
+++ manageuser.php
@@ -7,10 +7,8 @@ $action = getArrayVal($_GET, "action");
 $id = getArrayVal($_GET, "id");
 $mode = getArrayVal($_GET, "mode");
 
-if ($action != "login" and $action != "logout" and $action != "resetpassword" and $action != "loginerror")
-{
-    if (!isset($_SESSION["userid"]))
-    {
+if ($action != "login" and $action != "logout" and $action != "resetpassword" and $action != "loginerror") {
+    if (!isset($_SESSION["userid"])) {
         $template->assign("loginerror", 0);
         $template->display("login.tpl");
         die();
@@ -58,127 +56,51 @@ $mainclasses = array("desktop" => "",
 $template->assign("mainclasses", $mainclasses);
 $template->assign("classes", $classes);
 
-if ($action == "loginerror")
-{
-	$template->display("resetpassword.tpl");
-}
-elseif ($action == "resetpassword")
-{
-	$newpass = $user->resetPassword($email);
-	if ($newpass != "")
-	{
-		// Send e-mail with new password
-		$themail = new emailer($settings);
-		$themail->send_mail($email, $langfile["projectpasswordsubject"], $langfile["hello"] . ",<br /><br/>" . $langfile["projectpasswordtext"] . "<br /><br />" .  $langfile["newpass"] . ": " . "$newpass<br />" . $langfile["login"] . ": <a href = \"$url\">$url</a>");
+if ($action == "loginerror") {
+    $template->display("resetpassword.tpl");
+} elseif ($action == "resetpassword") {
+    $newpass = $user->resetPassword($email);
+    if ($newpass != "") {
+        // Send e-mail with new password
+        $themail = new emailer($settings);
+        $themail->send_mail($email, $langfile["projectpasswordsubject"], $langfile["hello"] . ",<br /><br/>" . $langfile["projectpasswordtext"] . "<br /><br />" . $langfile["newpass"] . ": " . "$newpass<br />" . $langfile["login"] . ": <a href = \"$url\">$url</a>");
 
-		$template->assign("success", 1);
-		$template->display("resetpassword.tpl");
-	}
-	else
-	{
-		$template->assign("loginerror", 1);
-		$template->display("resetpassword.tpl");
-	}
-}
-elseif ($action == "login")
-{
+        $template->assign("success", 1);
+        $template->display("resetpassword.tpl");
+    } else {
+        $template->assign("loginerror", 1);
+        $template->display("resetpassword.tpl");
+    }
+} elseif ($action == "login") {
     $mode = getArrayVal($_GET, "openid_mode");
     $username = getArrayVal($_POST, "username");
     $pass = getArrayVal($_POST, "pass");
-    // openid validation
-    if ($mode == "id_res")
-    {
-        $openid = new openid();
-        $openid->SetIdentity($_GET['openid_identity']);
-        $openid_validation_result = $openid->ValidateWithServer();
-        if ($openid_validation_result == true)
-        {
-            $name = getArrayVal($_REQUEST, "openid_sreg_fullname");
-            $lang = getArrayVal($_REQUEST, "openid_sreg_language");
-            $email = getArrayVal($_REQUEST, "openid_sreg_email");
-            // try to login using openid username w/o Pass
-            if ($user->login($name, ""))
-            {
-                $loc = $url . "index.php?mode=login";
-                header("Location: $loc");
-            }
-            else
-            {
-                // if user doesn't exist, create it and log it in
-                if ($user->add($name, $email, 0, "", 1, $lang))
-                {
-                    if ($user->login($name, ""))
-                    {
-                        $loc = $url . "index.php?mode=login";
-                        header("Location: $loc");
-                    }
-                }
-            }
-        }
-        else
-        {
-            $template->assign("loginerror", 1);
-            $template->display("login.tpl");
-        }
+    // normal login
+    if ($user->login($username, $pass)) {
+        $loc = $url . "index.php?mode=login";
+        header("Location: $loc");
+    } else {
+        $template->assign("loginerror", 1);
+        $template->display("login.tpl");
     }
-    else
-    {
-        // normal login
-        if ($user->login($username, $pass))
-        {
-            $loc = $url . "index.php?mode=login";
-            header("Location: $loc");
-        }
-        else
-        {
-            // openid
-            if (empty($pass) and stristr($username, "http"))
-            {
-                $openid = new openid();
-                $openid->SetIdentity($username);
-                $url = substr($url, 0, strlen($url)-1);
-                $openid->SetTrustRoot($url);
-                $openid->SetRequiredFields(array('email', 'fullname'));
-                $openid->SetOptionalFields(array('language', 'timezone'));
-                if ($openid->GetOpenIDServer())
-                {
-                    $openid->SetApprovedURL($url . "/manageuser.php?action=login"); // Send Response from OpenID server to this script
-                    $openid->Redirect(); // This will redirect user to OpenID Server
-                }
-            }
-            else
-            {
-                $template->assign("loginerror", 1);
-                $template->display("login.tpl");
-            }
-        }
-    }
-} elseif ($action == "logout")
-{
-    if ($user->logout())
-    {
+} elseif ($action == "logout") {
+    if ($user->logout()) {
         header("Location: index.php?mode=logout");
     }
-} elseif ($action == "addform")
-{
+} elseif ($action == "addform") {
     $title = $langfile['adduser'];
     $template->assign("title", $title);
     $template->display("adduserform.tpl");
-} elseif ($action == "editform")
-{
-	$languages_fin = array();
-    foreach($languages as $lang)
-    {
+} elseif ($action == "editform") {
+    $languages_fin = array();
+    foreach($languages as $lang) {
         $lang2 = $langfile[$lang];
         $fin = countLanguageStrings($lang);
 
-        if (!empty($lang2))
-        {
+        if (!empty($lang2)) {
             $lang2 .= " (" . $fin . "%)";
             $fin = array("val" => $lang, "str" => $lang2);
-        }
-        else
-        {
+        } else {
             $lang2 = $lang . " (" . $fin . "%)";
             $fin = array("val" => $lang, "str" => $lang2);
         }
@@ -193,13 +115,11 @@ elseif ($action == "login")
     $template->assign("user", $euser);
 
     $template->display("edituserform.tpl");
-} elseif ($action == "edit")
-{
+} elseif ($action == "edit") {
     $_SESSION['userlocale'] = $locale;
     $_SESSION['username'] = $name;
 
-    if (!empty($_FILES['userfile']['name']))
-    {
+    if (!empty($_FILES['userfile']['name'])) {
         $fname = $_FILES['userfile']['name'];
         $typ = $_FILES['userfile']['type'];
         $size = $_FILES['userfile']['size'];
@@ -207,22 +127,19 @@ elseif ($action == "login")
         $error = $_FILES['userfile']['error'];
         $root = "./";
 
-        $desc = $_POST['desc'];
         $teilnamen = explode(".", $fname);
         $teile = count($teilnamen);
         $workteile = $teile - 1;
         $erweiterung = $teilnamen[$workteile];
 
         $subname = "";
-        if ($typ != "image/jpeg" and $typ != "image/png" and $typ != "image/gif" and $typ != "image/pjpeg")
-        {
+        if ($erweiterung != "image/jpeg" and $erweiterung != "image/png" and $erweiterung != "image/gif" and $erweiterung != "image/pjpeg") {
             $loc = $url . "manageuser.php?action=profile&id=$userid";
             header("Location: $loc");
             die();
         }
 
-        for ($i = 0; $i < $workteile; $i++)
-        {
+        for ($i = 0; $i < $workteile; $i++) {
             $subname .= $teilnamen[$i];
         }
         list($usec, $sec) = explode(' ', microtime());
@@ -233,8 +150,7 @@ elseif ($action == "login")
         $subname = preg_replace("/[^-_0-9a-zA-Z]/", "_", $subname);
         $subname = preg_replace("/\W/", "", $subname);
 
-        if (strlen($subname) > 200)
-        {
+        if (strlen($subname) > 200) {
             $subname = substr($subname, 0, 200);
         }
 
@@ -242,54 +158,41 @@ elseif ($action == "login")
 
         $datei_final = CL_ROOT . "/files/" . CL_CONFIG . "/avatar/" . $fname;
 
-        if (move_uploaded_file($tmp_name, $datei_final))
-        {
+        if (move_uploaded_file($tmp_name, $datei_final)) {
             $avatar = $fname;
         }
 
-        if ($user->edit($userid, $name, $realname, $email, $tel1, $tel2, $company, $zip, $gender, $turl, $address1, $address2, $state, $country, "", $locale, $avatar, 0))
-        {
-            if (!empty($oldpass) and !empty($newpass) and !empty($repeatpass))
-            {
+        if ($user->edit($userid, $name, $realname, $email, $tel1, $tel2, $company, $zip, $gender, $turl, $address1, $address2, $state, $country, "", $locale, $avatar, 0)) {
+            if (!empty($oldpass) and !empty($newpass) and !empty($repeatpass)) {
+                $user->editpass($userid, $oldpass, $newpass, $repeatpass);
+            }
+            $loc = $url . "manageuser.php?action=profile&id=$userid&mode=edited";
+            header("Location: $loc");
+        }
+    } else {
+        if ($user->edit($userid, $name, $realname, $email, $tel1, $tel2, $company, $zip, $gender, $turl, $address1, $address2, $state, $country, "", $locale, "", 0)) {
+            if (isset($oldpass) and isset($newpass) and isset($repeatpass)) {
                 $user->editpass($userid, $oldpass, $newpass, $repeatpass);
             }
             $loc = $url . "manageuser.php?action=profile&id=$userid&mode=edited";
             header("Location: $loc");
         }
     }
-    else
-    {
-        if ($user->edit($userid, $name, $realname, $email, $tel1, $tel2, $company, $zip, $gender, $turl, $address1, $address2, $state, $country, "", $locale, "", 0))
-        {
-            if (isset($oldpass) and isset($newpass) and isset($repeatpass))
-            {
-                $user->editpass($userid, $oldpass, $newpass, $repeatpass);
-            }
-            $loc = $url . "manageuser.php?action=profile&id=$userid&mode=edited";
-            header("Location: $loc");
-        }
-    }
-} elseif ($action == "del")
-{
-    if ($user->del($id))
-    {
+} elseif ($action == "del") {
+    if ($user->del($id)) {
         $loc = $url . "admin.php?action=users&mode=deleted";
         header("Location: $loc");
     }
-} elseif ($action == "profile")
-{
+} elseif ($action == "profile") {
     $thetag = new tags();
     $start = getArrayVal($_GET, "start");
     $end = getArrayVal($_GET, "end");
     $proj = (object) new project();
-    if ($userpermissions["admin"]["add"])
-    {
+    if ($userpermissions["admin"]["add"]) {
         $projects = $proj->getMyProjects($id);
         $i = 0;
-        if (!empty($projects))
-        {
-            foreach($projects as $opro)
-            {
+        if (!empty($projects)) {
+            foreach($projects as $opro) {
                 $membs = $proj->getProjectMembers($opro["ID"], 1000);
                 $projects[$i]['members'] = $membs;
                 $i = $i + 1;
@@ -299,31 +202,22 @@ elseif ($action == "login")
     }
     $tracker = (object) new timetracker();
 
-	$track = array();
-    if (!empty($start) and !empty($end))
-    {
+    $track = array();
+    if (!empty($start) and !empty($end)) {
         $track = $tracker->getUserTrack($id, $fproject, $taski, $start, $end);
+    } elseif (is_array($fproject)) {
+        foreach ($fproject as $fpro) {
+            $ptrack = $tracker->getUserTrack($id, $fpro, $taski, $start, $end);
+            if (!empty($ptrack)) {
+                foreach ($ptrack as $mytrack) {
+                    array_push($track, $mytrack);
+                }
+            }
+        }
+    } else {
+        $track = $tracker->getUserTrack($id, $fproject, $taski);
     }
-    elseif(is_array($fproject))
-    {
-		foreach ($fproject as $fpro)
-		{
-			$ptrack = $tracker->getUserTrack($id, $fpro, $taski, $start, $end);
-			if(!empty($ptrack))
-			{
-			foreach ($ptrack as $mytrack)
-			{
-				array_push($track, $mytrack);
-			}
-			}
-		}
-    }
-    else
-    {
-		$track = $tracker->getUserTrack($id, $fproject, $taski);
-    }
-    if (!empty($track))
-    {
+    if (!empty($track)) {
         $totaltime = $tracker->getTotalTrackTime($track);
         $template->assign("totaltime", $totaltime);
         $template->assign("fproject", $fproject);
@@ -339,10 +233,8 @@ elseif ($action == "login")
     $template->assign("user", $profile);
 
     $template->display("userprofile.tpl");
-} elseif ($action == "showproject")
-{
-    if (!chkproject($userid, $id))
-    {
+} elseif ($action == "showproject") {
+    if (!chkproject($userid, $id)) {
         $errtxt = $langfile["notyourproject"];
         $noperm = $langfile["accessdenied"];
         $template->assign("mode", "error");
@@ -359,14 +251,12 @@ elseif ($action == "login")
     $alluser = $user->getAllUsers(10000);
     $users = array();
 
-    foreach($alluser as $all)
-    {
-        if (!chkproject($all['ID'], $id))
-        {
+    foreach($alluser as $all) {
+        if (!chkproject($all['ID'], $id)) {
             array_push($users, $all);
         }
     }
-	SmartyPaginate::disconnect();
+    SmartyPaginate::disconnect();
 
     $members = $proj->getProjectMembers($id, 14);
     $pro = $proj->getProject($id);
@@ -380,50 +270,37 @@ elseif ($action == "login")
     $template->assign("members", $members);
     $template->assign("users", $users);
     $template->display("projectmembers.tpl");
-} elseif ($action == "onlinelist")
-{
+} elseif ($action == "onlinelist") {
     $onlinelist = $user->getOnlinelist();
-    if (!empty($onlinelist))
-    {
+    if (!empty($onlinelist)) {
         echo "<ul>";
-        foreach($onlinelist as $online)
-        {
-            if ($online["avatar"])
-            {
+        foreach($onlinelist as $online) {
+            if ($online["avatar"]) {
                 $userpic = "thumb.php?pic=files/" . CL_CONFIG . "/avatar/$online[avatar]&width=90";
-            }
-            elseif ($online["gender"] == "f")
-            {
+            } elseif ($online["gender"] == "f") {
                 $userpic = "thumb.php?pic=templates/standard/images/no-avatar-female.jpg&amp;width=90";
-            }
-            else
-            {
+            } else {
                 $userpic = "thumb.php?pic=templates/standard/images/no-avatar-male.jpg&amp;width=90";
             }
             echo "<li>" . "<a class=\"user\" href = \"manageuser.php?action=profile&id=$online[ID]\">$online[name]<div><img src = \"$userpic\" /></div> </a>";
-            if ($online['ID'] != $userid and $userpermissions["chat"]["add"])
-            {
+            if ($online['ID'] != $userid and $userpermissions["chat"]["add"]) {
                 echo " <a class=\"chat\" href = \"javascript:openChatwin('$online[name]',$online[ID]);\" title=\"chat\"></a>";
-            }
-            elseif($online['ID'] == $userid and $userpermissions["chat"]["add"])
-            {
+            } elseif ($online['ID'] == $userid and $userpermissions["chat"]["add"]) {
                 echo " <a class=\"chat-user\" ></a>";
             }
             echo "</li>";
         }
         echo "</ul>";
     }
-}
-elseif ($action == "vcard")
-{
+} elseif ($action == "vcard") {
     $theuser = $user->getProfile($id);
 
     $vCard = (object) new vCard($theuser["locale"]);
 
     $vCard->setFirstName($theuser["name"]);
     $vCard->setCompany($theuser["company"]);
-	$vCard->setOrganisation($theuser["company"]);
-	$vCard->setPostalStreet($theuser["adress"]);
+    $vCard->setOrganisation($theuser["company"]);
+    $vCard->setPostalStreet($theuser["adress"]);
     $vCard->setPostalZIP($theuser["zip"]);
     $vCard->setWorkZIP($theuser["zip"]);
     $vCard->setHomeZIP($theuser["zip"]);
@@ -438,9 +315,10 @@ elseif ($action == "vcard")
     $vCard->setUrlWork($theuser["url"]);
     $vCard->setEMail($theuser["email"]);
 
-	header('Content-Type: text/x-vcard');
+    header('Content-Type: text/x-vcard');
     header('Content-Disposition: inline; filename=' . $theuser["name"] . '_' . date("d-m-Y") . '.vcf');
 
     echo $vCard->getCardOutput();
 }
+
 ?>
