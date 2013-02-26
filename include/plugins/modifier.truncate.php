38cede4ed294586f1005fcedcb34b92a9fce5c29
EvaK-de <eva@o-dyn.de>
2013-02-26 13:22:04 +0100

    Fixed bug in function for multibyte-safe truncation

--- include/plugins/modifier.truncate.php
+++ include/plugins/modifier.truncate.php
@@ -24,31 +24,23 @@
  * @param boolean
  * @return string
  */
-function smarty_modifier_truncate($string, $length = 80, $etc = '...', $break_words = false, $middle = false)
+function smarty_modifier_truncate($string, $length = 80, $etc = '...',
+                                  $break_words = false, $middle = false)
 {
     if ($length == 0)
-	{
         return '';
-	}
-
-    if (strlen($string) > $length)
-    {
-        $length -= min($length, strlen($etc));
-        if (!$break_words && !$middle)
-        {
-            $string = preg_replace('/\s+?(\S+)?$/', '', mb_substr($string, 0, $length+1));
+ 
+    if (mb_strlen($string) > $length) {
+        $length -= min($length, mb_strlen($etc));
+        if (!$break_words && !$middle) {
+            $string = preg_replace('/\s+?(\S+)?$/u', '', mb_substr($string, 0, $length+1, 'UTF-8'));
         }
-        if (!$middle)
-        {
-            return mb_substr($string, 0, $length) . $etc;
+        if(!$middle) {
+            return mb_substr($string, 0, $length, 'UTF-8') . $etc;
+        } else {
+            return mb_substr($string, 0, $length/2, 'UTF-8') . $etc . mb_substr($string, -$length/2, (mb_strlen($string)-$length/2), $charset);
         }
-        else
-        {
-            return mb_substr($string, 0, $length/2) . $etc . mb_substr($string, -$length/2);
-        }
-    }
-    else
-    {
+    } else {
         return $string;
     }
 }
