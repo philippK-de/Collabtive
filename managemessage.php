79da7b9293571538dd09c560939eef358c947d80
Philipp Kiszka <collabtive@o-dyn.de>
2014-06-16 07:17:23 +0200

    remove dead code

--- managemessage.php
+++ managemessage.php
@@ -204,8 +204,7 @@ if ($action == "addform") {
         die();
     }
 
-    $tagobj = new tags();
-    $tags = $tagobj->formatInputTags($tags);
+
     $themsg = $msg->add($id, $title, $message, $userid, $username, $mid_post, $milestone);
     if ($themsg) {
         if ($thefiles > 0) {
@@ -437,45 +436,6 @@ if ($action == "addform") {
     <tr><td >$message[text]</td></tr></table>";
     $pdf->writeHTML($htmltable, true, 0, true, 0);
     $pdf->Output("message$mid.pdf", "D");
-}
-elseif ($action == "mymsgs")
-{
-	// create new project and file objects
-	$project = new project();
-	$myfile = new datei();
-	// get all uof the users projects
-	$myprojects = $project->getMyProjects($userid);
-	$cou = 0;
-	$messages = array();
-	// loop through the projects and get messages and files for each project
-	if (!empty($myprojects))
-	{
-		foreach($myprojects as $proj)
-		{
-			$message = $msg->getProjectMessages($proj["ID"]);
-			$ordner = $myfile->getProjectFiles($proj["ID"], 1000);
-			$milestones = $objmilestone->getProjectMilestones($proj["ID"], 10000);
-			if(!empty($message))
-			{
-				array_push($messages,$message);
-			}
-			$myprojects[$cou]["milestones"] = $milestones;
-			$myprojects[$cou]["messages"] = $message;
-			$myprojects[$cou]["files"] = $ordner;
-			$cou = $cou + 1;
-		}
-	}
-	$emessages = reduceArray($messages);
-
-	// print_r($myprojects);
-	$title = $langfile['mymessages'];
-	$template->assign("title", $title);
-	$members = $project->getProjectMembers($id, 10000);
-	$template->assign("members", $members);
-	$template->assign("messages", $emessages);
-	$template->assign("msgnum", count($emessages));
-	$template->assign("myprojects", $myprojects);
-	$template->display("mymessages.tpl");
 }
 elseif ($action == "mymsgs-pdf") {
     $l = Array();
