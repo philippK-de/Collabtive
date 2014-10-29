f8e7cd9f8a918c8406f484ff0d1bc8c540d8eae3
Philipp Kiszka <info@o-dyn.de>
2014-10-29 08:46:55 +0100

    remove call to file_exists
    this impeded performance and was only used in the installer

--- include/initfunctions.php
+++ include/initfunctions.php
@@ -136,14 +136,16 @@ function getArrayVal(array $array, $name)
 {
     if (array_key_exists($name, $array)) {
     	$config = HTMLPurifier_Config::createDefault();
-    	if(file_exists(CL_ROOT . "/files/standard/ics"))
+    /*	if(file_exists(CL_ROOT . "/files/standard/ics"))
     	{
     		$config->set('Cache.SerializerPath', CL_ROOT . "/files/standard/ics");
     	}
     	else
     	{
     		$config->set('Cache.SerializerPath', NULL);
-    	}
+    	}*/
+
+		$config->set('Cache.SerializerPath', CL_ROOT . "/files/standard/ics");
 		$purifier = new HTMLPurifier($config);
         if (!is_array($array[$name])) {
             $clean = $purifier->purify($array[$name]);
