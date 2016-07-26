38d025a0b3d199af90df344a7741272ebdec7df3
Philipp Kiszka <collabtive@o-dyn.de>
2016-07-26 03:57:55 +0200

    improve plugin system
    scan /plugins/ dir for plugins automatically
    and provide registerPlugin() function for easy registering of plugins to
    tags with the template engine

--- include/class.plugins.php
+++ include/class.plugins.php
@@ -5,8 +5,31 @@ class plugins {
     private $installedPlugins;
 
     function __construct(){
-        $installedPluginsFile = file_get_contents(CL_ROOT . "/config/standard/installedPlugins.json");
-        $this->installedPlugins = json_decode($installedPluginsFile);
+        $this->installedPlugins = [];
+
+        //get the contents of the plugins folder
+        $pluginFiles = scandir(CL_ROOT . "/plugins/");
+        //loop through the contents
+        foreach ($pluginFiles as $pluginFile) {
+            if (!is_dir($pluginFile)) {
+                //magic number
+                //files are named plugin.name.php
+                //plugin. = 7 chars
+                //remove first 7 chars
+                $pluginName = substr($pluginFile, 7);
+                //magic number
+                //files are named plugin.name.php
+                //.php = 4 chars
+                //remove last 4 chars
+                $pluginName = substr($pluginName, 0, strlen($pluginName) - 4);
+
+                //push the plugin name to the list of installed plugins
+                array_push($this->installedPlugins, $pluginName);
+            }
+        }
+
+        //$installedPluginsFile = file_get_contents(CL_ROOT . "/config/standard/installedPlugins.json");
+        //$this->installedPlugins = json_decode($installedPluginsFile);
     }
     function loadPlugins()
     {
@@ -16,9 +39,14 @@ class plugins {
         return;
     }
 
-    function getTemplateFile()
+    function registerPlugin($templateTag, $templateCallable)
     {
+        global $template;
 
+        $template->registerPlugin("function", $templateTag, $templateCallable);
+
+        $template->registerFilter("pre", function ($source, Smarty_Internal_Template $templateObj) use ($templateTag) {
+            return preg_replace("/<!--" . $templateTag . "-->/i", "{{" . $templateTag . "}}", $source);
+        });
     }
-
 }
