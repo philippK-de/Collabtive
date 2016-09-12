279e64aaa7dac149b27ed13399aefd1a94de46d9
Philipp Kiszka <collabtive@o-dyn.de>
2016-09-12 18:16:35 +0200

    adapt to changed interface

--- plugins/plugin.test.php
+++ plugins/plugin.test.php
@@ -2,9 +2,15 @@
 
 class test implements collabtivePlugin
 {
-    const templateFile = "test.tpl";
-    const templateTag = "testplugin";
-    const templateTagTwo = "testpluginTwo";
+    private static $pluginHooks = [
+        "main" => "testplugin",
+        "second" => "testpluginTwo"
+    ];
+
+    private static $pluginTemplates = [
+        "main" => "test.tpl"
+    ];
+
     private $filterFunctions = ["test::activateMainHook", "test::activateSecondHook"];
 
     public function install()
@@ -16,26 +22,45 @@ class test implements collabtivePlugin
     {
         global $pluginManager;
         $templateTags = [
-            ["tag" => $this::templateTag],
-            ["tag" => $this::templateTagTwo]
+            ["tag" => test::$pluginHooks["main"]],
+            ["tag" => test::$pluginHooks["second"]]
         ];
         $pluginManager->registerPlugin($templateTags, "test");
         $pluginManager->registerHook($this->filterFunctions);
     }
 
+    /*
+     * Implement the main plugin hook
+     *
+     * This function is registered as a smarty pre-filter
+     * It is passed the unprocessed smarty template source, as well as an object representing the current template
+     * @implements collabtivePlugin
+     * @param str $source String representing the unmodified Smarty template, as read from the .tpl files
+     * @param obj $localTemplateObj Object representing the template
+     */
     static function activateMainHook($source, Smarty_Internal_Template $localTemplateObj)
     {
-        return preg_replace("/<!--" . test::templateTag . "-->/i", "{" . test::templateTag . "}", $source);
+        return preg_replace("/<!--" . test::$pluginHooks["main"] . "-->/i", "{" . test::$pluginHooks["main"] . "}", $source);
     }
 
     static function activateSecondHook($source, Smarty_Internal_Template $localTemplateObj)
     {
-        return preg_replace("/<!--" . test::templateTagTwo . "-->/i", "{" . test::templateTagTwo . "}", $source);
+        return preg_replace("/<!--" . test::$pluginHooks["second"] . "-->/i", "{" . test::$pluginHooks["second"] . "}", $source);
     }
 
+    /*
+     * Implement the main template. Each plugin has to have a main template
+     * This returns the string that is inserted for the {hook} created with activateHeaderHook.
+     * The return value is inserted into the $localTemplateObj at compile time
+     *
+     * This function is registered as a smarty plugin.
+     * It is passed parameters, as well as an object representing the current template
+     * @implements collabtivePlugin
+     * @param str $params Parameters bound to the plugin
+     * @param obj $localTemplateObj Object representing the template
+     */
     static function getMainTemplate($params, Smarty_Internal_Template $templateObj)
     {
-
         global $template;
 
         $taskObj = new project();
@@ -43,7 +68,7 @@ class test implements collabtivePlugin
 
         $template->assign("irgendwas", $allTask);
 
-        return $template->fetch(CL_ROOT . "/plugins/templates/" . test::templateFile);
+        return $template->fetch(CL_ROOT . "/plugins/templates/" . test::$pluginTemplates["main"]);
     }
 }
 
