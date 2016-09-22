d3651f2205644c138e5cc94e0669d82b7ee67b17
evaK-de <eva@o-dyn.de>
2016-09-22 02:44:32 +0200

    Give client role permission to view files and tasks upon initial creation during installation.

--- install.php
+++ install.php
@@ -19,7 +19,9 @@ setcookie("PHPSESSID", "");
 date_default_timezone_set("Europe/Berlin");
 $db_driver = "sqlite";
 require("./init.php");
+
 //error_reporting(0);
+
 $action = getArrayVal($_GET, "action");
 $locale = getArrayVal($_GET, "locale");
 
@@ -28,6 +30,7 @@ if (!empty($locale)) {
 } else {
     $locale = $_SESSION['userlocale'];
 }
+
 if (empty($locale)) {
     $locale = "en";
 }
@@ -49,7 +52,9 @@ if (!$action) {
     $configfilechk = is_writable(CL_ROOT . "/config/" . CL_CONFIG . "/config.php");
     $filesdir = is_writable(CL_ROOT . "/files/");
     $templatesdir = is_writable(CL_ROOT . "/templates_c/");
+
     $phpver = phpversion();
+
     $is_mbstring_enabled = extension_loaded('mbstring');
 
     $template->assign("phpver", $phpver);
@@ -60,19 +65,22 @@ if (!$action) {
 
     $template->display("install1.tpl");
 } elseif ($action == "step2") {
-    function randomPassword()
-    {
+    function randomPassword() {
         $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
         $pass = array(); //remember to declare $pass as an array
         $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+
         for ($i = 0; $i < 16; $i++) {
             $n = rand(0, $alphaLength);
             $pass[] = $alphabet[$n];
         }
+
         return implode($pass); //turn the array into a string
     }
+
 	//create a random password to encrypt files with
     $filePass = randomPassword();
+
     // check if the settings table / object is present. if yes, assume collabtive is already installed and abort
     if (!empty($settings)) {
         die("Collabtive seems to be already installed.<br />If this is an error, please clear your database.");
@@ -83,6 +91,7 @@ if (!$action) {
     $db_user = $_POST['db_user'];
     $db_pass = $_POST['db_pass'];
     $db_driver = $_POST['db_driver'];
+
     // write db login data to config file
     $file = fopen(CL_ROOT . "/config/" . CL_CONFIG . "/config.php", "w+");
     $str = "<?php
@@ -92,6 +101,7 @@ if (!$action) {
 \$db_pass = '$db_pass';\n
 \$db_driver = '$db_driver';\n
 ?>";
+
     $put = fwrite($file, "$str");
     if ($put) {
         @chmod(CL_ROOT . "/config/" . CL_CONFIG . "/config.php", 0755);
@@ -99,6 +109,7 @@ if (!$action) {
 
 	//this will be checked in install_mysql to make sure it can only be run from the installer
     $installer_include = "yes";
+
     // connect database.
     switch ($db_driver) {
         case "mysql":
@@ -108,18 +119,23 @@ if (!$action) {
             $conn = new PDO("sqlite:" . CL_ROOT . "/files/collabtive.sdb");
             break;
     }
+
     // Get the servers default timezone
     $timezone = date_default_timezone_get();
+
     // insert default settings
     $defSets = array("name" => "Collabtive", "subtitle" => "Projectmanagement", "locale" => $locale, "timezone" => $timezone, "dateformat" => "d.m.Y", "template" => "standard", "mailnotify" => 1, "mailfrom" => "collabtive@localhost", "mailfromname" => "", "mailmethod" => "mail", "mailhost" => "", "mailuser" => "", "mailpass" => "", "rssuser" => "", "rsspass" => "", "theme" => "standard", "filePass" => $filePass);
+
     foreach($defSets as $setKey => $setVal) {
         $ins = $conn->query("INSERT INTO settings (`settingsKey`,`settingsValue`) VALUES ('$setKey','$setVal')");
     }
+
     if (!$ins) {
         $template->assign("errortext", "Error: Failed to create initial settings.");
         $template->display("error.tpl");
         die();
     }
+
     $template->display("install2.tpl");
 } elseif ($action == "step3") {
 	//create required folders
@@ -129,6 +145,7 @@ if (!$action) {
 
 	//include the database cnfig
     require(CL_ROOT . "/config/" . CL_CONFIG . "/config.php");
+
     // Start database connection
     switch ($db_driver) {
         case "mysql":
@@ -141,6 +158,7 @@ if (!$action) {
 
     $user = $_POST["name"];
     $pass = $_POST["pass"];
+
     // create the first user
     $usr = new user();
 
@@ -158,14 +176,42 @@ if (!$action) {
         $template->display("error.tpl");
         die();
     }
+
     // insert default roles
     $rolesobj = new roles();
 
-    $adminrid = $rolesobj->add("Admin", array("add" => 1, "edit" => 1, "del" => 1 , "close" => 1, "view" => 1), array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1), array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1), array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1), array("add" => 1, "edit" => 1, "del" => 1, "view" => 1), array("add" => 1, "edit" => 1, "del" => 1, "read" => 1, "view" => 1), array("add" => 1), array("add" => 1));
+    $adminrid = $rolesobj->add("Admin",                                         // name
+        array("add" => 1, "edit" => 1, "del" => 1 , "close" => 1, "view" => 1), // projects
+        array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1),  // tasks
+        array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1),  // milestones
+        array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1),  // messages
+        array("add" => 1, "edit" => 1, "del" => 1, "view" => 1),                // files
+        array("add" => 1, "edit" => 1, "del" => 1, "read" => 1, "view" => 1),   // time tracking
+        array("add" => 1),                                                      // chat
+        array("add" => 1)                                                       // administration
+    );
 
-    $userrid = $rolesobj->add("User", array("add" => 1, "edit" => 1, "del" => 0, "close" => 0, "view" => 1), array("add" => 1, "edit" => 1, "del" => 0, "close" => 1, "view" => 1), array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1), array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1), array("add" => 1, "edit" => 1, "del" => 1, "view" => 1), array("add" => 1, "edit" => 1, "del" => 1, "read" => 0, "view" => 1), array("add" => 1), array("add" => 0));
+    $userrid = $rolesobj->add("User",                                            // name
+        array("add" => 1, "edit" => 1, "del" => 0, "close" => 0, "view" => 1),   // projects
+        array("add" => 1, "edit" => 1, "del" => 0, "close" => 1, "view" => 1),   // tasks
+        array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1),   // milestones
+        array("add" => 1, "edit" => 1, "del" => 1, "close" => 1, "view" => 1),   // messages
+        array("add" => 1, "edit" => 1, "del" => 1, "view" => 1),                 // files
+        array("add" => 1, "edit" => 1, "del" => 1, "read" => 0, "view" => 1),    // time tracking
+        array("add" => 1),                                                       // chat
+        array("add" => 0)                                                        // administration
+    );
 
-    $clientrid = $rolesobj->add("Client", array("add" => 0, "edit" => 0, "del" => 0, "close" => 0), array("add" => 0, "edit" => 0, "del" => 0, "close" => 0), array("add" => 0, "edit" => 0, "del" => 0, "close" => 0), array("add" => 0, "edit" => 0, "del" => 0, "close" => 0), array("add" => 0, "edit" => 0, "del" => 0), array("add" => 0, "edit" => 0, "del" => 0, "read" => 0), array("add" => 0), array("add" => 0));
+    $clientrid = $rolesobj->add("Client",                                        // name
+        array("add" => 0, "edit" => 0, "del" => 0, "close" => 0, "view" => 0),   // projects
+        array("add" => 0, "edit" => 0, "del" => 0, "close" => 0, "view" => 1),   // tasks
+        array("add" => 0, "edit" => 0, "del" => 0, "close" => 0, "view" => 0),   // milestones
+        array("add" => 0, "edit" => 0, "del" => 0, "close" => 0, "view" => 0),   // messages
+        array("add" => 0, "edit" => 0, "del" => 0, "view" => 1),                 // files
+        array("add" => 0, "edit" => 0, "del" => 0, "read" => 0, "view" => 0),    // time tracking
+        array("add" => 0),                                                       // chat
+        array("add" => 0)                                                        // administration
+    );
 
     if (!$adminrid or !$userrid or !$clientrid) {
         $template->assign("errortext", "Error: Failed to create initial roles.");
